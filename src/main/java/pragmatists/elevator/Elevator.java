88bcafcb04ab008b87673f8459e476cb92bfcc55
Tomasz Kamiński <kaminski.tomasz.a@gmail.com>
2017-09-26 12:18:04 +0200

    kata-26-09-2017: refactoring elevator states

--- src/main/java/pragmatists/elevator/Elevator.java
+++ src/main/java/pragmatists/elevator/Elevator.java
@@ -11,15 +11,11 @@ import pragmatists.elevator.panel.ButtonListener;
 public class Elevator implements
         ButtonListener, DoorListener, EngineListener {
 
-    private enum ElevatorState {WAITING, GOING_UP, GOING_DOWN}
-    private ElevatorState state = ElevatorState.WAITING;
-
     private final Door door;
     private final Engine engine;
-
+    private ElevatorState elevatorState = ElevatorState.WAITING;
     private Floor currentFloor = Floor.ofLevel(0);
     private Floor requestedFloor;
-
 
     public Elevator(Door door, Engine engine) {
         this.door = door;
@@ -27,6 +23,7 @@ public class Elevator implements
         this.door.setListener(this);
         this.engine.setListener(this);
     }
+
 
     public Elevator(Door door, Engine engine, Floor startingFloor) {
         this(door, engine);
@@ -40,13 +37,13 @@ public class Elevator implements
 
     @Override
     public void floorRequested(Floor floor) {
-        if (state == ElevatorState.WAITING) {
+        if (elevatorState == ElevatorState.WAITING) {
             requestedFloor = floor;
             if (floor.isGreaterThan(currentFloor)) {
-                state = ElevatorState.GOING_UP;
+                elevatorState = ElevatorState.GOING_UP;
             }
             if (floor.isLowerThan(currentFloor)) {
-                state = ElevatorState.GOING_DOWN;
+                elevatorState = ElevatorState.GOING_DOWN;
             }
             door.close();
         }
@@ -55,21 +52,9 @@ public class Elevator implements
     @Override
     public void doorStateChanged(DoorState doorState) {
         if (doorState == DoorState.OPENED) {
-
-            state = ElevatorState.WAITING;
-            requestedFloor = null;
-
+            elevatorState = ElevatorState.WAITING;
         } else if (doorState == DoorState.CLOSED) {
-
-            if (requestedFloor == null) {
-                return;
-            }
-
-            if (state == ElevatorState.GOING_UP) {
-                engine.start(Direction.UP);
-            } else if (state == ElevatorState.GOING_DOWN) {
-                engine.start(Direction.DOWN);
-            }
+            engine.start(elevatorState.direction());
         }
     }
 
@@ -95,5 +80,22 @@ public class Elevator implements
         return "Elevator{" +
                 "currentFloor=" + currentFloor +
                 '}';
+    }
+
+    private enum ElevatorState {
+
+        WAITING(Direction.NONE),
+        GOING_UP(Direction.UP),
+        GOING_DOWN(Direction.DOWN),;
+
+        private final Direction direction;
+
+        ElevatorState(Direction direction) {
+            this.direction = direction;
+        }
+
+        public Direction direction() {
+            return direction;
+        }
     }
 }
